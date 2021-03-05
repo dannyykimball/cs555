1e2ecbf0ad7626e794c2a06b9c1a00a0b79e650c
Daniel Kimball <dannyykimball@gmail.com>
2021-03-05 16:49:07 -0500

    hashmap done

--- GedcomParser.js
+++ GedcomParser.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { builtinModules } = require("module");
+
 const TAGS = [
   "INDI",
   "NAME",
@@ -20,19 +20,15 @@ const TAGS = [
   "NOTE",
 ];
 
-let hashmap = new Map();
-let prevID = "";
-let prevFirst = "";
-let currentPerson = {
-  name: "",
-  gender: "",
-  birthday: "",
-  age: "",
-  alive: "",
-  death: "",
-  child: [],
-  spouse: [],
-};
+function currentFamily() {
+  this.ID = "";
+  this.Married = "";
+  (this.Divorced = "N/A"), (this.HusbandID = "");
+  this.HusbandName = "";
+  this.WifeID = "";
+  this.WifeName = "";
+  Children = [];
+}
 
 //run this to test the code
 //File to be parsed must be in the same folder as this
@@ -43,50 +39,75 @@ function parseFile(fileName) {
     if (e) throw e;
     fileText = data.toString();
     fileText = fileText.split("\r\n");
+    let segments = [];
+    let segment = "";
     for (let i = 0; i < fileText.length - 1; i++) {
-      //   console.log("--> " + fileText[i]);
-      output(fileText[i]);
+      if (fileText[i].includes("INDI")) {
+        segments.push(segment);
+        segment = "";
+      }
+      segment = segment + fileText[i] + " ";
     }
+    segments.shift();
+
+    let hashmap = new Map();
+
+    for (i in segments) {
+      hashmap = output(segments[i], hashmap);
+    }
+
+    console.log(hashmap.get("I20").Name);
+    return hashmap;
   });
 }
 
 //helper function
-function output(text) {
+function output(text, hashmap) {
   //splits the lines into an array of strings, then makes sure the order is right and returns the formatted version
   let textArr = text.split(" ");
-  let op = "<-- ";
-  let valid = "N";
-
-  if (textArr.includes("INDI")) {
-    op = op.concat(textArr[0] + "|" + "INDI" + "|" + "Y");
-    textArr.splice(textArr.indexOf("INDI"), 1);
-  } else if (textArr.includes("FAM")) {
-    op = op.concat(textArr[0] + "|" + "FAM" + "|" + "Y");
-    textArr.splice(textArr.indexOf("FAM"), 1);
-  } else {
-    if (TAGS.includes(textArr[1])) valid = "Y";
-    op = op.concat(textArr[0] + "|" + textArr[1] + "|" + valid);
-    textArr.splice(1, 1);
+  // console.log(textArr);
+  let person = {
+    ID: "",
+    Name: "",
+    Gender: "",
+    Birthday: "",
+    Age: "",
+    Alive: true,
+    Death: "",
+    SpouseFamily: [],
+    ChildOfFamily: [],
+  };
+  for (let i = 0; i < textArr.length; i++) {
+    if (textArr[i].includes("INDI")) {
+      person.ID = textArr[i - 1].replace("@", "").replace("@", "");
+    } else if (textArr[i] == "NAME") {
+      person.Name = textArr[i + 1];
+      i++;
+    } else if (textArr[i] == "SURN") {
+      person.Name = person.Name + " " + textArr[i + 1];
+      i++;
+    } else if (textArr[i] == "SEX") {
+      person.Gender = textArr[i + 1];
+      i++;
+    } else if (textArr[i] == "BIRT") {
+      person.Birthday =
+        textArr[i + 3] + " " + textArr[i + 4] + " " + textArr[i + 5];
+      i = i + 5;
+    } else if (textArr[i] == "DEAT") {
+      person.Alive = false;
+      person.Death =
+        textArr[i + 4] + " " + textArr[i + 5] + " " + textArr[i + 6];
+      i = i + 6;
+    } else if (textArr[i].includes("FAMS")) {
+      person.SpouseFamily = textArr[i + 1].replace("@", "").replace("@", "");
+    } else if (textArr[i].includes("FAMC")) {
+      person.ChildOfFamily = textArr[i + 1].replace("@", "").replace("@", "");
+    }
   }
-  if (textArr.length > 1) op = op.concat("|");
 
-  for (let i = 1; i < textArr.length; i++) {
-    op = op.concat(textArr[i]) + " ";
-  }
+  hashmap.set(person.ID, person);
 
-  if (op.includes("INDI")) {
-    prevID = textArr[1].replace("@", "").replace("@", "");
-  }
-
-  if (op.includes("GIVN")) {
-    prevFirst = textArr[1];
-  }
-
-  if (op.includes("SURN")) {
-    hashmap.set(prevID, prevFirst + " " + textArr[1]);
-    console.log(hashmap.get(prevID));
-    console.log(hashmap);
-  }
+  return hashmap;
 }
 
 parseFile("MyFamily.ged");
